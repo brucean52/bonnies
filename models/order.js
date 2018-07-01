fb651a7bf850b94e0adf8905bc7695da09c01f84
Eeps30 <eeps30@gmail.com>
2018-07-01 01:02:01 -0700

    changed order of order list headers around

--- models/order.js
+++ models/order.js
@@ -1,7 +1,10 @@
 let mongoose = require('mongoose');
 
 let orderSchema = mongoose.Schema({
-
+    id: Number,
+    itemsOrdered: Array,
+    timeOrdered: Number,
+    
 });
 
 module.exports = mongoose.model('Order', orderSchema);
