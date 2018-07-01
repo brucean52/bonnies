4da1853934181c18b788444200d70e839e42d800
Eeps30 <eeps30@gmail.com>
2018-06-30 18:30:03 -0700

    created data pull for menu items on one of the routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const {MongoClient, ObjectID} = require('mongodb');
+
 var router = express.Router({
     mergeParams: true
 });
@@ -11,4 +13,28 @@ router.get("/", function(req, res) {
 router.get("/1", function(req, res) {
   res.render("index1");
 });
+
+// GET all orders from the orders table 
+router.get("/orders", function(req, res) {
+  
+  var orderData = MongoClient.connect('mongodb://eeps30:av862549@ds125001.mlab.com:25001/bonnies_vegan_cuisine', (err, db) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server.');
+
+    db.collection('menu_item').find().toArray().then((docs) => {
+        console.log('Menu Items: ');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch menu items', err);
+    })
+    
+    // db.close();
+  })
+
+  res.render('orderData: ', {orderData});
+
+})
+
 module.exports = router;
